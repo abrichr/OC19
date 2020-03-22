d082661a2503ac87d7f29a8239fadac476e721fd
Richard Abrich <richard.abrich@gmail.com>
2020-03-22 19:29:54 -0400

    Remove unused Project class

--- app/project/__init__.py
+++ app/project/__init__.py
@@ -1,12 +1,7 @@
 from flask import Blueprint
 
+
 project_blueprint = Blueprint('project', __name__)
-
-
-class Project():
-    def __init__(self, user, name):
-        self.user = user 
-        self.name = name
 
 
 from app.project import routes
