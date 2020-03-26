40a7c11ad7f91dc0244feea50e8eaacaca2fb3e6
Richard Abrich <richard.abrich@gmail.com>
2020-03-25 23:11:08 -0400

    Improve account view; update title

--- app/views/main.py
+++ app/views/main.py
@@ -5,4 +5,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template(
+        'index.html',
+        title='OnCOVID19 - Find problems, form teams, build solutions'
+    )
